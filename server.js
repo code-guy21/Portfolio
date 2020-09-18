f4acd33afc73dffaa3b4488bf546636c474ee321
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-09-18 18:19:30 -0400

    changed main route path to /

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/html/index.html"));
 });
 
